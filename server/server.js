1fde54cf93cd5f5dab141bf134feb1cb78f57f6f
uriel.laguna <181544@utags.edu.mx>
2020-12-03 17:02:23 -0600

    Creacion de una carpeta de rutas

--- server/server.js
+++ server/server.js
@@ -7,61 +7,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
 app.get('/', function (req, res) {
   res.send('<h2>Bienvenido a mi servidor REST (localhost)</h2>')
 });
 
-app.get('/usuario', function (req, res) {
-  res.json({
-    ok: '200',
-    mensaje: 'Usuarios insertados con exito'
-  })
-});
-
-
-app.post('/usuario', function (req, res) {
-  let nombre = req.body.nombre;
-  let body = req.body;
-
-  if(nombre === undefined){
-    res.status(400).json({
-        ok: 400,
-        mensaje: 'Favor de enviar el valor del nombre'
-    });
-  }else{
-
-      res.json({
-        ok: 200,
-        mensaje: 'Usuario insertado con éxito',
-        body: body
-      });
-    
-    }  
-});
-
-
-app.put('/usuario/:id/:nombre', function (req, res) {
-    let id = req.params.id;
-    let nombre = req.params.nombre;
-
-    res.json({
-      ok: '200',
-      mensaje: 'Usuario actualizado con éxito',
-      id: id,
-      nombre: nombre
-    })
-  
-});
-
-app.delete('/usuario/:id', function (req, res){
-    let id = req.params.id;
-
-    res.json({
-      ok: 200,
-      mensaje: 'Usuario eliminiado con éxito',
-      id: id
-    });
-});
+app.use(require('./routes/usuario'));
 
 mongoose.connect('mongodb://localhost:27017/cafeteria',{
   useNewUrlParser: true,
